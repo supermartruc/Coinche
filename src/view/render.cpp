0ae611664fa2a6a5e552cd03c06156b02a3a3cd9
Adrien <adrien.dubois@ens-lyon.fr>
2023-03-01 18:32:57 +0100

    meerdeuh

--- src/view/render.cpp
+++ src/view/render.cpp
@@ -71,9 +71,9 @@ void	GameView::clear() {
 void	GameView::renderPaquet(Paquet paquet) {
 	int i = 0;
 	float temp = hWindow / ( (float) ((paquet.size()+0.5)*wCarte) );
-	float chev = (float) std::min((float) 1, (float) (temp * 4/5.0) );
+	float chev = 1; //(float) std::min((float) 1, (float) (temp * 4/5.0) );
 	int wStart = (wWindow - (chev*wCarte * paquet.size())) / 2;
-	Paquet copy_paquet = tri_paquet_affichage(paquet, Atout::Sa);
+	Paquet copy_paquet = tri_paquet_affichage(paquet, Atout::Ta);
 	for (auto &carte : copy_paquet) {
 		renderCarte(carte, wStart + (i * chev * wCarte), hWindow-hCarte);
 		i++;
