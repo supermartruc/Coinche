e235a32e6cd87882598709ea6cf5f59d5b530336
Frederic Becerril <frederic.becerril@buzzee.fr>
2023-03-01 13:32:48 +0100

    old version card

--- deleteBlack.py
+++ deleteBlack.py
@@ -1,46 +1,25 @@
 from PIL import Image
 
-def findFirstNoneBlackPixelX(image, width, height):
+def replace_black_pixels(image_path):
+	# Open the image and get its dimensions
+	image = Image.open(image_path)
+	width, height = image.size
+
+	# Create a new transparent image with the same dimensions
+	new_image = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+
+	# Loop through each pixel in the original image and copy it to the new image,
+	# unless it's a black pixel at the edge of the image
 	for x in range(width):
 		for y in range(height):
 			pixel = image.getpixel((x, y))
-			if pixel[0] != 0 or pixel[1] != 0 or pixel[2] != 0:
-				return x
-	return width
-
-def findFirstNoneBlackPixelY(image, width, height):
-	for y in range(height):
-		for x in range(width):
-			pixel = image.getpixel((x, y))
-			if pixel[0] != 0 or pixel[1] != 0 or pixel[2] != 0:
-				return y
-	return height
-
-def findLastNoneBlackPixelX(image, width, height):
-	for x in range(width - 1, -1, -1):
-		for y in range(height):
-			pixel = image.getpixel((x, y))
-			if pixel[0] != 0 or pixel[1] != 0 or pixel[2] != 0:
-				return x
-	return 0
-
-def findLastNoneBlackPixelY(image, width, height):
-	for y in range(height - 1, -1, -1):
-		for x in range(width):
-			pixel = image.getpixel((x, y))
-			if pixel[0] != 0 or pixel[1] != 0 or pixel[2] != 0:
-				return y
-	return 0
-
-def delete_black_pixels(image_path):
-	image = Image.open(image_path)
-	width, height = image.size
-	x1 = findFirstNoneBlackPixelX(image, width, height)
-	y1 = findFirstNoneBlackPixelY(image, width, height)
-	x2 = findLastNoneBlackPixelX(image, width, height)
-	y2 = findLastNoneBlackPixelY(image, width, height)
-	image = image.crop((x1, y1, x2, y2))
-	image.save(image_path)
+			if (x <= 4 or x >= width - 5) or (y <= 4 or y >= height - 5):
+				if pixel[0] != 0 or pixel[1] != 0 or pixel[2] != 0:
+					new_image.putpixel((x, y), pixel)
+			else:
+				new_image.putpixel((x, y), pixel)
+	# Save the new image
+	new_image.save(image_path)
 
 
 Couleur = ["Coeur", "Carreau", "Trefle", "Pique"]
@@ -48,4 +27,6 @@ Valeur = ["As", "Sept", "Huit", "Neuf", "Dix", "Valet", "Dame", "Roi"]
 
 for i in range(4):
 	for j in range(8):
-		delete_black_pixels('ressources/'+Couleur[i]+'/'+Valeur[j]+'.png')
+		replace_black_pixels('ressources/'+Couleur[i]+'/'+Valeur[j]+'.png')
+
+replace_black_pixels('ressources/Pique/Dame.png')
